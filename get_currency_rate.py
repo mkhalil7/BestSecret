a4b8dfecb158068ff00177c813bc2d1f1c342388
mkhalil7 <mk.ouachikh@gmail.com>
2021-11-26 17:23:45 +0100

    Working version

--- get_currency_rate.py
+++ get_currency_rate.py
@@ -3,10 +3,11 @@
 
 
 # the configatuin parametrs are present in the config.ini file
-
 import pandas as pd
 import requests
 import configparser
+import sqlite3
+from sqlite3 import Error
 
 
 # reading the config file
@@ -25,11 +26,16 @@ api_key = config.get('api', 'token')
 api_url = config.get('api', 'url')
 
 
+# SQlLite parameters
+db_name = config.get('sqlLite', 'db_name')
+
+
 # send an API request and add the data
-def query_api():
+def get_currency_api_data():
 
     if(len(api_key) == 0 or len(api_url) == 0):
-        raise Exception('Missing api URL or token please check the configuration file')
+        raise Exception(
+            'Missing api URL or token please check the configuration file')
 
     params = {
         'access_key': api_key,
@@ -38,45 +44,66 @@ def query_api():
         'end_date': end_date
     }
 
-    #sending the request to the API 
-    response = requests.get(api_url,params = params)
+    # sending the request to the API
+    response = requests.get(api_url, params=params)
     response_json = response.json()
-    
-   # print(response_json)
-    if(response.status_code == 200 and response_json['success'] or True):
 
-        #loading everything to data frame 
-        df = pd.DataFrame.from_dict(pd.json_normalize(response_json),  orient='columns')
+    if(response.status_code == 200 and response_json['success']):
 
-        #removing unecessary columns 
+        # loading everything to data frame
+        df = pd.DataFrame.from_dict(
+        pd.json_normalize(response_json),  orient='columns')
+
+        # removing unecessary columns
         del df['success']
         del df['timeseries']
         del df['start_date']
         del df['end_date']
 
-
-        #using melt to unpivot table
+        # using melt to unpivot table
         df = pd.melt(df, id_vars=["base"], var_name="temp", value_name="Value")
-        df[['Rate','Date', 'Currency']] = df['temp'].str.split('.', 2, expand=True)
+        df[['Rate', 'Date', 'Currency']] = df['temp'].str.split(
+            '.', 2, expand=True)
 
-        #cleaning and reorganizing 
-        df.columns = df.columns.str.replace('base', 'Base')
-        df = df[['Date','Base', 'Currency', 'Value']]
-   
-        print(df)
-    else:   
-        raise Exception('API query failed with folowing error message :' + response_json['error']['info'])
+        #renaming and reorganizing
+        df.columns = df.columns.str.replace('base', 'base_code')
+        df.columns = df.columns.str.replace('Date', 'date')
+        df.columns = df.columns.str.replace('Currency', 'currency_code')
+        df.columns = df.columns.str.replace('Value', 'rate')
 
+        df = df[['base_code', 'date', 'rate', 'currency_code']]
 
-    
-   # print(api_token, api_url)
+        return df
 
-    # to be fixed
+    else:
+        raise Exception(
+            'API query failed with folowing error message :' + response_json['error']['info'])
 
 
 # store the data into a sqlite database
-def store_date_sqlite():
-    pass
+def store_date_sqlite(df):
+
+    conn = None
+    try:
+        conn = sqlite3.connect(db_name)
+    except Error as e:
+        print(e)
+
+    df.to_sql('currency_conversion', conn, if_exists='append', index=False)
+    return conn
 
 
-query_api()
+def get_currency_average(currency, date_start, date_end, conn):
+    cur = conn.cursor()
+    cur.execute("SELECT AVG(rate) FROM currency_conversion WHERE currency_code=? AND date > ? AND date < ?", (currency,date_start,date_end))
+    result = cur.fetchone()[0]
+    print (f" The exchange rate for {currency} between {date_start} and {date_end} is : {result:.4f}")
+
+#Getting Data From APi
+df = get_currency_api_data()
+
+#Writing To SQLLite
+conn = store_date_sqlite(df)
+
+# function to calcuate the average
+get_currency_average("MAD", "2021-11-19", "2021-11-26", conn)
