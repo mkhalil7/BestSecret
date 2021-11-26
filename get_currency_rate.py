95f058f24d2560d40ab36428e67286b05dffed72
mkhalil7 <mk.ouachikh@gmail.com>
2021-11-26 12:53:41 +0100

    Quick changes

---
+++ get_currency_rate.py
@@ -0,0 +1,36 @@
+# The goal of this class is to provide a pipeline to retireve historical exchange rate data 
+# The base currency as well as the start and end date can be configured as parameters
+
+
+# imports
+
+
+
+
+#pipeline constant parameters 
+currency = "Eur"
+start_date = "25/11/2021"
+end_date = "26/11/2021"
+
+
+#api parameters 
+token = ""
+path  = ""
+url= ""
+
+#send an API request and add the data 
+def read_data():
+    pass
+
+
+#store the data into a sqlite database
+def store_date_sqlite():
+    pass
+
+
+
+
+
+
+
+
